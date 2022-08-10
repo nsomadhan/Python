8c965087d965a0e2e7987ac9102751fadc02f830
nsomadhan <nsomadhan@gmail.com>
2022-08-10 15:10:37 +0600

    strip with function

--- strip.py
+++ strip.py
@@ -13,3 +13,29 @@ print( name.strip('$') )
 name =   "  $$$john$$$$    " 
 stripname = name.strip() # First removeing white space 
 print(stripname.strip('$')) # the print with removing $
+
+def namestrip():
+   stripwhitespace = name.strip().strip('$')
+   print(stripwhitespace)
+
+print ('With function')
+namestrip()
+
+
+print ('variable pass With function')
+name = '  $$variable$$ '
+def varnamestrip(name):
+   stripwhitespace = name.strip().strip('$')
+   return stripwhitespace
+print(varnamestrip(name))
+
+
+
+
+#print("striping with for loop")
+
+#for namestrip in name:
+#    namestrip.strip()
+#    namestrip.strip('$')
+#    print(namestrip)
+
