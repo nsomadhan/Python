7e40671258a8412f56db54f0fe8f94ef1c698621
nsomadhan <nsomadhan@gmail.com>
2022-08-10 12:47:52 +0600

    strip function with whitespace

--- strip.py
+++ strip.py
@@ -1,2 +1,11 @@
 name = "       john         "
-print( name.strip( ) ) # Striping all space
+print( name.strip() ) # Striping all space
+
+name =   "$$$john$$$$"  # no white space 
+print( name.strip('$') )
+
+name =   "$$$john$$$$  " # white space
+print( name.strip('$') )
+
+name =   "  $$$john$$$$"  # white space 
+print( name.strip('$') )
